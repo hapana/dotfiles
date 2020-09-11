de756d7649b55823f90a8e7e746332f340cc02dd
Harry P <hpanay@gmail.com>
2020-09-11 11:56:04 +0100

    Fix tmux config and start it

--- start.sh
+++ start.sh
@@ -15,6 +15,9 @@ docker run \
   -v $(pwd)/config/zsh-scripts:/home/$USERNAME/.oh-my-zsh/custom \
   -v $(pwd)/config/task/taskrc:/home/$USERNAME/.taskrc \
   -v $(pwd)/config/fzf/fzf.zsh:/home/$USERNAME/.fzf.zsh \
+  -v $(pwd)/config/zsh/zsh_history:/home/$USERNAME/.zsh_history \
+  -v $(pwd)/config/zsh/zshrc:/home/$USERNAME/.zshrc \
+  -v $(pwd)/config/tmux/tmux.conf:/home/$USERNAME/.tmux.conf \
   --rm \
   -it home \
-  /bin/zsh
+  tmux
